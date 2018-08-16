595b8bfa5c07d0fbb5e0d1ffc7b8a8e2febe7611
jose gallegos <mini_jg@Bruces-Mac-mini.local>
2018-08-16 14:20:53 -0600

    test commit2

---
+++ fractionOps.cpp
@@ -0,0 +1,56 @@
+// chapter 1
+
+/*
+ * Write a fractionClass which defines adding, subtraction, multiplying,
+ * and dividing fractions by overloading standard operators for these 
+ * operations.  Write a function method for reducing factors and over-
+ * load I/O operators for input and output.
+ */
+
+// step one:
+// use struct or class
+
+// step two:
+// define operations on fractions
+
+// step three:
+// embed a method in the class
+
+// step four:
+// ensure the method o'erloads input fractions
+
+// step five:
+// ensure the method o'erloads ouput fractions
+
+
+// step six:
+// consider the user
+
+// step seven:
+// input output
+
+// step 8
+// 
+
+// name the program and compile it using this command
+// 	> gcc -o fractionOps fractionOps.cpp
+
+// run it with 
+//	> ./fractionOps
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{    
+    int number;
+ 
+    cout << "Enter an integer: ";
+    cin >> number;
+ 
+    cout << "You entered " << number;    
+    return 0;
+}
+
+
+	
