72e82a8067293bad0a790a3e94a419b387be61f4
jose <josezilla303@gmail.com>
2018-08-30 12:48:56 -0600

    quick2.py is th same program as quickSortFromWWW.py, but without the comments

--- pythonProgs/quick2.py
+++ pythonProgs/quick2.py
@@ -1,4 +1,5 @@
 # quick2.py
+
 def quickSort(alist):
    quickSortHelper(alist,0,len(alist)-1)
 
@@ -43,3 +44,4 @@ def partition(alist,first,last):
 alist = [54,26,93,17,77,31,44,55,20]
 quickSort(alist)
 print(alist)
+s
